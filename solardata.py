e94159c56c2145f494f71cfa8313003c1e4a1671
simpsoti <46614269+simpsoti@users.noreply.github.com>
2019-09-29 14:09:06 -0600

    Solarpi

---
+++ solardata.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import solaredge
+import json
+
+class SolarData:
+    def __init__(self, api_key, site_id):
+        self.__solarEdge = solaredge.Solaredge(api_key)
+        self.__data = self.__solarEdge.get_overview(site_id)
+
+    def lifetime_energy(self):
+        return round(self.__data["overview"]["lifeTimeData"]["energy"]/1000, 2)
+    
+    def last_year_energy(self):
+        return round(self.__data["overview"]["lastYearData"]["energy"]/1000, 2)
+    
+    def last_day_energy(self):
+        return round(self.__data["overview"]["lastDayData"]["energy"]/1000, 2)
+    
+    def current_power(self):
+        return round(self.__data["overview"]["currentPower"]["power"]/1000, 2)
+    
+    
+    
+
+
+
